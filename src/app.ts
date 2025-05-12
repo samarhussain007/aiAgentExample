159b66fb4ea1b4d5930fefd816f743671d64a35d
samarhussain007 <hussainsamar36@gmail.com>
2025-05-13 01:26:02 +0530

    create a rag model using langgraph

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,79 @@
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
-import { ChatOllama } from "@langchain/ollama";
+import { ChatOllama, OllamaEmbeddings } from "@langchain/ollama";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { pull } from "langchain/hub";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { Annotation, StateGraph } from "@langchain/langgraph";
+import { Document } from "@langchain/core/documents";
 
 const model = new ChatOllama({
   model: "llama2",
 });
 
-/*
-const messages = [
-  new SystemMessage("Translate the following English into Italian"),
-  new HumanMessage("hi!"),
-];
-
-const stream = await model.stream(messages);
-
-const chunks = [];
-for await (const chunk of stream) {
-  chunks.push(chunk);
-  console.log(`${chunk.content}|`);
-}
-*/
-
-const systemTemplate = "Translate the following English into {language}";
-
-const promptTemplate = ChatPromptTemplate.fromMessages([
-  ["system", systemTemplate],
-  ["user", "{text}"],
-]);
-const promptValue = await promptTemplate.invoke({
-  language: "italian",
-  text: "hi!",
+const embeddings = new OllamaEmbeddings({
+  model: "llama2",
 });
 
-const response = await model.invoke(promptValue);
+const vectorStore = new MemoryVectorStore(embeddings);
 
-console.log(response.content);
+const pTagSelector = "p";
+const cheerioLoader = new CheerioWebBaseLoader(
+  "https://lilianweng.github.io/posts/2023-06-23-agent/",
+  {
+    selector: pTagSelector,
+  }
+);
+
+const docs = await cheerioLoader.load();
+
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 200,
+});
+
+const allSplits = await splitter.splitDocuments(docs);
+
+await vectorStore.addDocuments(allSplits);
+
+const promptTemplate = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+
+const InputStateAnnotation = Annotation.Root({
+  question: Annotation<string>,
+});
+
+const StateAnnotation = Annotation.Root({
+  question: Annotation<string>,
+  context: Annotation<Document[]>,
+  answer: Annotation<string>,
+});
+
+// Define application steps
+const retrieve = async (state: typeof InputStateAnnotation.State) => {
+  const retrievedDocs = await vectorStore.similaritySearch(state.question);
+  return { context: retrievedDocs };
+};
+
+const generate = async (state: typeof StateAnnotation.State) => {
+  const docsContent = state.context.map((doc) => doc.pageContent).join("\n");
+  const messages = await promptTemplate.invoke({
+    question: state.question,
+    context: docsContent,
+  });
+  const response = await model.invoke(messages);
+  return { answer: response.content };
+};
+
+// Compile application and test
+const graph = new StateGraph(StateAnnotation)
+  .addNode("retrieve", retrieve)
+  .addNode("generate", generate)
+  .addEdge("__start__", "retrieve")
+  .addEdge("retrieve", "generate")
+  .addEdge("generate", "__end__")
+  .compile();
+
+let inputs = { question: "What is Task Decomposition?" };
+
+const result = await graph.invoke(inputs);
+console.log(result.answer);
