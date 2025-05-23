8eb03ac809a5dfd22e02b4fedd6569a3cdfd3d56
samarhussain007 <hussainsamar36@gmail.com>
2025-05-23 18:01:29 +0530

    created the retriever node

--- src/app.ts
+++ src/app.ts
@@ -18,13 +18,19 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StateGraph } from "@langchain/langgraph";
 import { TaskType } from "@google/generative-ai";
 
-import { ChatOllama } from "@langchain/ollama";
+import { ChatOllama, OllamaEmbeddings } from "@langchain/ollama";
+import { DocumentInterface } from "@langchain/core/documents";
 
 const urls = [
   "https://lilianweng.github.io/posts/2023-06-23-agent/",
   "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
   "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
 ];
+
+const model = new ChatOllama({
+  model: "llama3.1:8b",
+  temperature: 0,
+});
 
 const docs = await Promise.all(
   urls.map(async (url) => {
@@ -36,325 +42,55 @@ const docs = await Promise.all(
   })
 );
 
-const docsList = docs.flat();
+const allDocs = docs.flat();
 
 const textsplitters = new RecursiveCharacterTextSplitter({
-  chunkSize: 500,
-  chunkOverlap: 50,
+  chunkSize: 1000,
+  chunkOverlap: 200,
 });
 
-const docsSplit = await textsplitters.splitDocuments(docsList);
+const docsSplit = await textsplitters.splitDocuments(allDocs);
 
-const vectoreStore = await MemoryVectorStore.fromDocuments(
+const vectorStore = await MemoryVectorStore.fromDocuments(
   docsSplit,
-  new GoogleGenerativeAIEmbeddings({
-    model: "gemini-embedding-exp-03-07",
-    taskType: TaskType.SEMANTIC_SIMILARITY,
-    apiKey: process.env.GOOGLE_API_KEY,
+  new OllamaEmbeddings({
+    model: "llama3.1:8b",
   })
 );
 
-const retriever = vectoreStore.asRetriever();
-
-// Creating a tool for retriever
-const retrieverTool = createRetrieverTool(retriever, {
-  name: "retrieve_blog_posts",
-  description:
-    "Search and return information about Lilian Weng blog posts on LLM agents, prompt engineering, and adversarial attacks on LLMs.",
-});
-
-const tools = [retrieverTool];
-const toolNode = new ToolNode<typeof GraphState.State>(tools);
+const retriever = vectorStore.asRetriever();
 
 const GraphState = Annotation.Root({
-  messages: Annotation<BaseMessage[]>({
-    reducer: (x, y) => x.concat(y),
-    default: () => [],
+  documents: Annotation<DocumentInterface[]>({
+    reducer: (x, y) => y ?? x ?? [],
+  }),
+  question: Annotation<string>({
+    reducer: (x, y) => y ?? x ?? "",
+  }),
+  generation: Annotation<string>({
+    reducer: (x, y) => y ?? x,
   }),
 });
 
-//functions
-
 /**
- * Decides whether the agent should retrieve more information or end the process.
- * This function checks the last message in the state for a function call. If a tool call is
- * present, the process continues to retrieve information. Otherwise, it ends the process.
- * @param {typeof GraphState.State} state - The current state of the agent, including all messages.
- * @returns {string} - A decision to either "continue" the retrieval process or "end" it.
- */
-
-function shouldRetrieve(state: typeof GraphState.State): string {
-  const { messages } = state;
-  console.log("---DECIDE TO RETRIEVE---");
-  const lastMessage = messages[messages.length - 1];
-
-  if (
-    "tool_calls" in lastMessage &&
-    Array.isArray(lastMessage.tool_calls) &&
-    lastMessage.tool_calls.length
-  ) {
-    console.log("---DECISION: RETRIEVE---");
-    return "retrieve";
-  }
-  // If there are no tool calls then we finish.
-  return END;
-}
-
-/**
- * Determines whether the Agent should continue based on the relevance of retrieved documents.
- * This function checks if the last message in the conversation is of type FunctionMessage, indicating
- * that document retrieval has been performed. It then evaluates the relevance of these documents to the user's
- * initial question using a predefined model and output parser. If the documents are relevant, the conversation
- * is considered complete. Otherwise, the retrieval process is continued.
- * @param {typeof GraphState.State} state - The current state of the agent, including all messages.
- * @returns {Promise<Partial<typeof GraphState.State>>} - The updated state with the new message added to the list of messages.
- */
-
-async function gradeDocuments(
-  state: typeof GraphState.State
-): Promise<Partial<typeof GraphState.State>> {
-  console.log("---GET RELEVANCE---");
-  const { messages } = state;
-  const tool = {
-    name: "give_relevance_score",
-    description: "Give a relevance score to the retrieved documents.",
-    schema: z.object({
-      binaryScore: z.string().describe("Relevance score 'yes' or 'no'"),
-    }),
-  };
-
-  const prompt = ChatPromptTemplate.fromTemplate(
-    `You are a grader assessing relevance of retrieved docs to a user question.
-  Here are the retrieved docs:
-  \n ------- \n
-  {context} 
-  \n ------- \n
-  Here is the user question: {question}
-  If the content of the docs are relevant to the users question, score them as relevant.
-  Give a binary score 'yes' or 'no' score to indicate whether the docs are relevant to the question.
-  Yes: The docs are relevant to the question.
-  No: The docs are not relevant to the question.`
-  );
-
-  const model = new ChatGoogleGenerativeAI({
-    model: "gemini-2.0-flash",
-    temperature: 0,
-    apiKey: process.env.GOOGLE_API_KEY,
-  }).bindTools([tool], {
-    tool_choice: tool.name,
-  });
-
-  const chain = prompt.pipe(model);
-
-  const lastMessage = messages[messages.length - 1];
-
-  const score = await chain.invoke({
-    question: messages[0].content as string,
-    context: lastMessage.content,
-  });
-
-  console.log("---SCORE---");
-  console.log(score);
-
-  return {
-    messages: [score],
-  };
-}
-
-/**
- * Check the relevance of the previous LLM tool call.
+ * Retrieve documents
  *
- * @param {typeof GraphState.State} state - The current state of the agent, including all messages.
- * @returns {string} - A directive to either "yes" or "no" based on the relevance of the documents.
+ * @param {typeof GraphState.State} state The current state of the graph.
+ * @param {RunnableConfig | undefined} config The configuration object for tracing.
+ * @returns {Promise<Partial<typeof GraphState.State>>} The new state object.
  */
 
-function checkRelevance(state: typeof GraphState.State): string {
-  console.log("---CHECK RELEVANCE---");
-
-  const { messages } = state;
-  const lastMessage = messages[messages.length - 1];
-  if (!("tool_calls" in lastMessage)) {
-    throw new Error(
-      "The 'checkRelevance' node requires the most recent message to contain tool calls."
-    );
-  }
-  const toolCalls = (lastMessage as AIMessage).tool_calls;
-  if (!toolCalls || !toolCalls.length) {
-    throw new Error("Last message was not a function message");
-  }
-
-  if (toolCalls[0].args.binaryScore === "yes") {
-    console.log("---DECISION: DOCS RELEVANT---");
-    return "yes";
-  }
-  console.log("---DECISION: DOCS NOT RELEVANT---");
-  return "no";
-}
-
-// Nodes
-
-/**
- * Invokes the agent model to generate a response based on the current state.
- * This function calls the agent model to generate a response to the current conversation state.
- * The response is added to the state's messages.
- * @param {typeof GraphState.State} state - The current state of the agent, including all messages.
- * @returns {Promise<Partial<typeof GraphState.State>>} - The updated state with the new message added to the list of messages.
- */
-
-async function agent(
+async function retrieve(
   state: typeof GraphState.State
 ): Promise<Partial<typeof GraphState.State>> {
-  console.log("---CALL AGENT---");
-  const { messages } = state;
-
-  // Find the AIMessage which contains the `give_relevance_score` tool call,
-  // and remove it if it exists. This is because the agent does not need to know
-  // the relevance score.
-
-  const filteredMessages = messages.filter(
-    (message) =>
-      !(
-        message instanceof AIMessage &&
-        message.tool_calls &&
-        message.tool_calls[0].name === "give_relevance_score"
-      )
-  );
-
-  const model = new ChatGoogleGenerativeAI({
-    model: "gemini-2.0-flash",
-    temperature: 0,
-    apiKey: process.env.GOOGLE_API_KEY,
-    streaming: true,
-  }).bindTools(tools);
-
-  const response = await model.invoke(filteredMessages);
-
+  console.log("Retrieving documents...");
+  const { question } = state;
+  const documents = await retriever
+    .withConfig({
+      runName: "FetchRelevantDocs",
+    })
+    .invoke(question);
   return {
-    messages: [response],
+    documents,
   };
 }
-
-/**
- * Transform the query to produce a better question.
- * @param {typeof GraphState.State} state - The current state of the agent, including all messages.
- * @returns {Promise<Partial<typeof GraphState.State>>} - The updated state with the new message added to the list of messages.
- */
-
-async function rewrite(
-  state: typeof GraphState.State
-): Promise<Partial<typeof GraphState.State>> {
-  console.log("REWRITE QUERY");
-  const { messages } = state;
-  const question = messages[0].content as string;
-  const prompt = ChatPromptTemplate.fromTemplate(
-    `Look at the input and try to reason about the underlying semantic intent / meaning. \n 
-Here is the initial question:
-\n ------- \n
-{question} 
-\n ------- \n
-Formulate an improved question:`
-  );
-
-  const model = new ChatGoogleGenerativeAI({
-    model: "gemini-2.0-flash",
-    temperature: 0,
-    apiKey: process.env.GOOGLE_API_KEY,
-  });
-
-  const chain = prompt.pipe(model);
-
-  const response = await chain.invoke({ question });
-  return {
-    messages: [response],
-  };
-}
-
-/**
- * Generate answer
- * @param {typeof GraphState.State} state - The current state of the agent, including all messages.
- * @returns {Promise<Partial<typeof GraphState.State>>} - The updated state with the new message added to the list of messages.
- */
-
-async function generate(
-  state: typeof GraphState.State
-): Promise<Partial<typeof GraphState.State>> {
-  console.log("---GENERATE ANSWER---");
-  const { messages } = state;
-  const question = messages[0].content as string;
-  const lastToolMessage = messages
-    .slice()
-    .reverse()
-    .find((message) => message.getType() === "tool");
-
-  const prompt = await pull<ChatPromptTemplate>("rlm/rag-prompt");
-
-  const model = new ChatGoogleGenerativeAI({
-    model: "gemini-2.0-flash",
-    temperature: 0,
-    apiKey: process.env.GOOGLE_API_KEY,
-    streaming: true,
-  }).bindTools(tools);
-
-  const chain = prompt.pipe(model);
-
-  const response = await chain.invoke({
-    question,
-    context: lastToolMessage?.content,
-  });
-
-  return {
-    messages: [response],
-  };
-}
-
-// Define the graph
-const workflow = new StateGraph(GraphState)
-  // Define the nodes which we'll cycle between.
-  .addNode("agent", agent)
-  .addNode("retrieve", toolNode)
-  .addNode("gradeDocuments", gradeDocuments)
-  .addNode("rewrite", rewrite)
-  .addNode("generate", generate);
-
-workflow.addEdge(START, "agent").addConditionalEdges("agent", shouldRetrieve);
-
-workflow.addEdge("retrieve", "gradeDocuments");
-
-workflow.addConditionalEdges("gradeDocuments", checkRelevance, {
-  yes: "generate",
-  no: "rewrite",
-});
-workflow.addEdge("rewrite", "agent");
-
-workflow.addEdge("generate", END);
-
-const app = workflow.compile();
-
-const inputs = {
-  messages: [
-    new HumanMessage(
-      "What are the types of agent memory based on Lilian Weng's blog post?"
-    ),
-  ],
-};
-
-let finalState;
-
-for await (const output of await app.stream(inputs)) {
-  for (const [key, value] of Object.entries(output)) {
-    const lastMsg = output[key].messages[output[key].messages.length - 1];
-    console.log(`Output from node: '${key}'`);
-    console.dir(
-      {
-        type: lastMsg.getType(),
-        content: lastMsg.content,
-        tool_calls: lastMsg.tool_calls,
-      },
-      { depth: null }
-    );
-    console.log("---\n");
-    finalState = value;
-  }
-}
-
-console.log(JSON.stringify(finalState, null, 2));
