12b08b0f117a1b3527736782e932e28cee9e3eba
samarhussain007 <hussainsamar36@gmail.com>
2025-05-23 18:41:37 +0530

    created additional functions and tools

--- src/app.ts
+++ src/app.ts
@@ -14,12 +14,15 @@ import { Annotation, END, Graph, START } from "@langchain/langgraph";
 import { AIMessage, BaseMessage, HumanMessage } from "@langchain/core/messages";
 import { z } from "zod";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { formatDocumentsAsString } from "langchain/util/document";
 
 import { StateGraph } from "@langchain/langgraph";
 import { TaskType } from "@google/generative-ai";
 
 import { ChatOllama, OllamaEmbeddings } from "@langchain/ollama";
 import { DocumentInterface } from "@langchain/core/documents";
+import { tool } from "@langchain/core/tools";
 
 const urls = [
   "https://lilianweng.github.io/posts/2023-06-23-agent/",
@@ -92,5 +95,133 @@ async function retrieve(
     .invoke(question);
   return {
     documents,
+  };
+}
+
+async function generate(
+  state: typeof GraphState.State
+): Promise<Partial<typeof GraphState.State>> {
+  console.log("---GENERATE---");
+
+  const prompt = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+
+  const { documents, question } = state;
+
+  const ragChain = prompt.pipe(model).pipe(new StringOutputParser());
+
+  const generation = await ragChain.invoke({
+    context: formatDocumentsAsString(documents),
+    question: question,
+  });
+
+  return {
+    generation,
+  };
+}
+
+async function gradeDocuments(
+  state: typeof GraphState.State
+): Promise<Partial<typeof GraphState.State>> {
+  console.log("---CHECK RELEVANCE---");
+
+  const gradeSchema = z
+    .object({
+      binaryScore: z
+        .enum(["yes", "no"])
+        .describe("Relevance score 'yes' or 'no'"),
+    })
+    .describe(
+      "Grade the relevance of the retrieved documents to the question."
+    );
+
+  //@ts-ignore
+  const llmWithTool = model.withStructuredOutput(gradeSchema, {
+    name: "grade",
+  });
+
+  const prompt = ChatPromptTemplate.fromTemplate(
+    `You are a grader assessing relevance of a retrieved document to a user question.
+  Here is the retrieved document:
+
+  {context}
+
+  Here is the user question: {question}
+
+  If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant.
+  Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.`
+  );
+
+  // Chain
+  const chain = prompt.pipe(llmWithTool);
+
+  const filteredDocs: Array<DocumentInterface> = [];
+  for await (const doc of state.documents) {
+    const grade = await chain.invoke({
+      context: doc.pageContent,
+      question: state.question,
+    });
+    if (grade.binaryScore === "yes") {
+      console.log("---GRADE: DOCUMENT RELEVANT---");
+      filteredDocs.push(doc);
+    } else {
+      console.log("---GRADE: DOCUMENT NOT RELEVANT---");
+    }
+  }
+
+  return {
+    documents: filteredDocs,
+  };
+}
+
+/**
+ * Transform the query to produce a better question.
+ *
+ * @param {typeof GraphState.State} state The current state of the graph.
+ * @param {RunnableConfig | undefined} config The configuration object for tracing.
+ * @returns {Promise<Partial<typeof GraphState.State>>} The new state object.
+ */
+
+async function transformQuery(state: typeof GraphState.State) {
+  console.log("---TRANSFORM QUERY---");
+
+  // Pull in the prompt
+  const prompt = ChatPromptTemplate.fromTemplate(
+    `You are generating a question that is well optimized for semantic search retrieval.
+  Look at the input and try to reason about the underlying sematic intent / meaning.
+  Here is the initial question:
+  \n ------- \n
+  {question} 
+  \n ------- \n
+  Formulate an improved question: `
+  );
+
+  const chain = prompt.pipe(model).pipe(new StringOutputParser());
+
+  const betterQuestion = await chain.invoke({ question: state.question });
+
+  return {
+    question: betterQuestion,
+  };
+}
+
+/**
+ * Web search based on the re-phrased question using Tavily API.
+ *
+ * @param {typeof GraphState.State} state The current state of the graph.
+ * @param {RunnableConfig | undefined} config The configuration object for tracing.
+ * @returns {Promise<Partial<typeof GraphState.State>>} The new state object.
+ */
+async function webSearch(
+  state: typeof GraphState.State
+): Promise<Partial<typeof GraphState.State>> {
+  console.log("---WEB SEARCH---");
+
+  const tvlyTool = tool();
+  // const docs = await tool.invoke({ input: state.question });
+  // const webResults = new Document({ pageContent: docs });
+  // const newDocuments = state.documents.concat(webResults);
+
+  return {
+    documents: newDocuments,
   };
 }
